ea3a2f84edd52fe1e8f67e07cd48041dda53ed8e
IngaGd <inga.gudaite@gmail.com>
2024-11-11 12:12:50 +0200

    Add drop down table aparence

--- limo-shop-app/src/pages/home/Home.tsx
+++ limo-shop-app/src/pages/home/Home.tsx
@@ -1,14 +1,13 @@
-import DescriptionTableDropDown from "../../common/components/DescriptionTableDropDown";
 import ProductGroupBox from "../../common/components/ProductGroupBox";
 import styles from "./home.module.scss";
 
 export default function Home() {
   const homeClass = `${styles.home}`;
+
   return (
     <>
       <div className={homeClass}>
         <ProductGroupBox />
-        <DescriptionTableDropDown />
       </div>
     </>
   );
