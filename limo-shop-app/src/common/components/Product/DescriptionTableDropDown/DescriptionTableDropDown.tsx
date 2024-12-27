131513439abfda942347038405674b2684b2e341
IngaGd <inga.gudaite@gmail.com>
2024-12-27 16:04:26 +0700

    Add functionality for setting quantity to 1 after adding to cart

--- limo-shop-app/src/common/components/Product/DescriptionTableDropDown/DescriptionTableDropDown.tsx
+++ limo-shop-app/src/common/components/Product/DescriptionTableDropDown/DescriptionTableDropDown.tsx
@@ -2,6 +2,9 @@ import { Button } from "components/Button";
 import styles from "./descriptionTableDropDown.module.scss";
 import { Link } from "react-router-dom";
 import { ProductPropsTypes } from "../product.types";
+import { GlobalContext } from "src/common/context/GlobalContext";
+import { GlobalContextType } from "src/common/context/globalContext.types";
+import { useContext } from "react";
 
 const text1 = "Kiekis";
 const text2 = "Kaina";
@@ -10,17 +13,40 @@ const currency = "Eur";
 const plusIcon = "+";
 const minusIcon = "-";
 
-export function DescriptionTableDropDown({
-  productList,
-  addToCart,
-  handleIncrement,
-  productQuantity,
-  handleDecrement,
-}: ProductPropsTypes) {
+export function DescriptionTableDropDown({ product }: ProductPropsTypes) {
+  const {
+    handleIncrement,
+    handleDecrement,
+    addToCart,
+    quantities,
+    setQuantities,
+  } = useContext(GlobalContext) as GlobalContextType;
+
+  const productQuantity =
+    quantities.find((element) => element.id === product.id)?.qty || 1;
+
+  const handleAddToCart = (p: {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    imagePath: string;
+  }) => {
+    addToCart(p);
+    setQuantities(
+      quantities.map((item) => {
+        return {
+          ...item,
+          qty: 1,
+        };
+      })
+    );
+  };
+
   return (
     <div className={`${styles.table}`}>
-      <p className={`${styles.description}`}>{productList.description}</p>
-      <Link to={`/items/${productList.id}`} className={styles.link}>
+      <p className={`${styles.description}`}>{product.description}</p>
+      <Link to={`/items/${product.id}`} className={styles.link}>
         Plačiau
       </Link>
       <div className={`${styles.purchasing}`}>
@@ -28,23 +54,22 @@ export function DescriptionTableDropDown({
           <div> {text1}</div>
           <div className={`${styles.details}`}>
             <div>{productQuantity}</div>
-            <div onClick={handleIncrement}>{plusIcon}</div>
-            <div onClick={handleDecrement}>{minusIcon}</div>
+            <div onClick={() => handleIncrement(product.id)}>{plusIcon}</div>
+            <div onClick={() => handleDecrement(product.id)}>{minusIcon}</div>
           </div>
         </div>
         <div className={`${styles.price}`}>
           <div> {text2}</div>
           <div className={`${styles.details}`}>
-            <div>
-              {productQuantity
-                ? productQuantity * productList.price
-                : productList.price}
-            </div>
+            <div>{productQuantity * product.price}</div>
             <div>{currency}</div>
           </div>
         </div>
         <Link to={"/cart"}>
-          <Button buttonLabel={buttonText} handleClick={addToCart} />
+          <Button
+            buttonLabel={buttonText}
+            handleClick={() => handleAddToCart(product)}
+          />
         </Link>
       </div>
     </div>
