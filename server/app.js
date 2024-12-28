c2b4087591b3b2614e730a4f35d16b00260a9573
IngaGd <inga.gudaite@gmail.com>
2024-12-28 16:30:22 +0700

    Add express server

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+const port = 8080;
+
+const corsOptions = {
+  origin: "http://wdp.lt/",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.static(path.join(__dirname, "../limo-shop-app/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../limo-shop-app/dist", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`App is listening op port: ${port}`);
+});
