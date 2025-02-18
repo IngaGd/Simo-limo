0d7c058c54879c532e01c17320255c3497ce7d7f
IngaGd <inga.gudaite@gmail.com>
2025-02-18 14:44:51 +0700

    Refactor server code by adding routes to order and products, removing redundant code

--- server/server.js
+++ server/server.js
@@ -1,19 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const helmet = require("helmet");
-const { google } = require("googleapis");
 const { v4: uuidv4 } = require("uuid");
 const cors = require("cors");
 const path = require("path");
-const fs = require("fs");
 const bodyParser = require("body-parser");
-const createDOMPurify = require("dompurify");
-const { JSDOM } = require("jsdom");
-const { body, validationResult } = require("express-validator");
-const { log } = require("util");
+// const createDOMPurify = require("dompurify");
+// const { JSDOM } = require("jsdom");
+//const { body, validationResult } = require("express-validator");
+//const { log } = require("util");
 
-const window = new JSDOM("").window;
-const DOMPurify = createDOMPurify(window);
+// const window = new JSDOM("").window;
+// const DOMPurify = createDOMPurify(window);
 
 const app = express();
 const port = 8080;
@@ -44,201 +42,120 @@ app.use(
   })
 );
 
-const pathToJson = path.join(__dirname, "limo.json");
-const credentials = JSON.parse(fs.readFileSync(pathToJson));
+//Routes
+app.use("/api", require("./routes/products"));
+// app.get("/api/products", async (req, res) => {
+//   console.log("Request received on /api/products");
+//   try {
+//     console.log("Spreadsheet ID:", PRODUCT_LIST_ID);
 
-//const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+//     const response = await sheets.spreadsheets.values.get({
+//       spreadsheetId: PRODUCT_LIST_ID,
+//       range: "Products!A2:E3",
+//     });
 
-const auth = new google.auth.GoogleAuth({
-  credentials,
-  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+//     console.log("API Response:", response.data);
+
+//     const rows = response.data.values;
+//     if (rows && rows.length) {
+//       console.log("Rows data: ", rows);
+//       res.status(200).json(rows);
+//     } else {
+//       res.status(404).send({ message: "No data found" });
+//     }
+//   } catch (error) {
+//     console.error(
+//       "Error reading data: ",
+//       error.response?.data || error.message
+//     );
+//     res.status(500).send("Server error");
+//   }
+// });
+
+app.use("/api", require("./routes/orders"));
+
+// app.post(
+//   "/api/order",
+//  ,
+//   async (req, res) => {
+// console.log("Request received on /api/order");
+// const orderNo = uuidv4();
+
+// const { products, purchaser, paymentStatus } = req.body;
+// console.log(
+//   "Payload being sent to Google Sheets:",
+//   purchaser.firstName,
+//   purchaser.termsConfirmed,
+//   products[0].title
+// );
+
+// const arrOfProducts = [];
+// products.forEach((product) => {
+//   arrOfProducts.push([
+//     product.id,
+//     product.title,
+//     product.quantity,
+//     product.totalPrice,
+//   ]);
+// });
+// console.log("arrOfProducts: ", arrOfProducts);
+
+// const orderValues = (arr1, arr2, status) => {
+//   let arr = [];
+//   for (const element of arr2) {
+//     arr.push(arr1.concat(element).concat(status));
+//   }
+//   return arr;
+// };
+
+// try {
+//   await sheets.spreadsheets.values.append({
+//     spreadsheetId: PRODUCT_LIST_ID,
+//     range: "Orders!A2",
+//     valueInputOption: "USER_ENTERED",
+//     insertDataOption: "INSERT_ROWS",
+//     resource: {
+//       majorDimension: "ROWS",
+//       values: orderValues(
+//         [
+//           DOMPurify.sanitize(purchaser.firstName),
+//           DOMPurify.sanitize(purchaser.lastName),
+//           DOMPurify.sanitize(purchaser.phone),
+//           DOMPurify.sanitize(purchaser.email),
+//           DOMPurify.sanitize(purchaser.street),
+//           DOMPurify.sanitize(purchaser.town),
+//           DOMPurify.sanitize(purchaser.postCode),
+//           orderNo,
+//           purchaser.termsConfirmed,
+//         ],
+//         arrOfProducts,
+//         paymentStatus
+//       ),
+//     },
+//   });
+//   res.status(200).json({
+//     status: 200,
+//     message: "Užsakymas apdorojamas",
+//     orderId: `${orderNo}`,
+//     paymentStatus: "pending",
+//     redirectToPayment: true,
+//   });
+// } catch (error) {
+//   console.error("Error updating data:", error);
+//   res.status(500).json("Error updating data");
+// }
+//   }
+// );
+
+app.post("/api/create-transaction", async (req, res) => {
+  console.log("Request received on /api/create-transaction");
+  const { orderId } = req.body;
+  console.log("orderId: ", orderId);
 });
 
-const sheets = google.sheets({ version: "v4", auth });
-const PRODUCT_LIST_ID = process.env.PRODUCT_LIST_SPREADSHEET_ID;
-console.log("Spreadsheet ID:", PRODUCT_LIST_ID);
-
-app.get("/api/products", async (req, res) => {
-  console.log("Request received on /api/products");
-  try {
-    console.log("Spreadsheet ID:", PRODUCT_LIST_ID);
-
-    const response = await sheets.spreadsheets.values.get({
-      spreadsheetId: PRODUCT_LIST_ID,
-      range: "Products!A2:E3",
-    });
-
-    console.log("API Response:", response.data);
-
-    const rows = response.data.values;
-    if (rows && rows.length) {
-      console.log("Rows data: ", rows);
-      res.status(200).json(rows);
-    } else {
-      res.status(404).send({ message: "No data found" });
-    }
-  } catch (error) {
-    console.error(
-      "Error reading data: ",
-      error.response?.data || error.message
-    );
-    res.status(500).send("Server error");
-  }
-});
-
-app.post(
-  "/api/order",
-  [
-    body("purchaser.firstName")
-      .notEmpty()
-      .withMessage("Privalomas laukas")
-      .bail()
-      .isLength({ min: 2, max: 50 })
-      .withMessage("Vardas gali būti nuo 2 iki 50 simbolių.")
-      .bail()
-      .isString()
-      .matches(/^[a-zA-ZąčęėįšųūžĄČĘĖĮŠŲŪŽ' -]+$/)
-      .withMessage("Pašalinkite negalimus simbolius, galimi - raidės, (-), (')")
-      .trim()
-      .escape(),
-    body("purchaser.lastName")
-      .notEmpty()
-      .withMessage("Privalomas laukas")
-      .bail()
-      .isLength({ min: 2, max: 50 })
-      .withMessage("Vardas gali būti nuo 2 iki 50 simbolių.")
-      .bail()
-      .isString()
-      .matches(/^[a-zA-ZąčęėįšųūžĄČĘĖĮŠŲŪŽ' -]+$/)
-      .withMessage("Pašalinkite negalimus simbolius, galimi - raidės, (-), (')")
-      .trim()
-      .escape(),
-    body("purchaser.phone")
-      .notEmpty()
-      .withMessage("Privalomas laukas")
-      .bail()
-      .matches(/^\+?[0-9]{1,4}[0-9]{6,14}$/)
-      .withMessage("Telefono numerio pavyzdys (pavyzdys: +370656789)."),
-    body("purchaser.email")
-      .notEmpty()
-      .withMessage("Privalomas laukas")
-      .bail()
-      .isEmail()
-      .normalizeEmail()
-      .matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/),
-    body("purchaser.street")
-      .notEmpty()
-      .withMessage("Privalomas laukas")
-      .bail()
-      .isLength({ min: 2, max: 50 })
-      .withMessage("Gatvės pavadinimas gali būti nuo 2 iki 50 simbolių.")
-      .bail()
-      .isString()
-      .matches(/^[a-zA-ZąčęėįšųūžĄČĘĖĮŠŲŪŽ' -]+$/)
-      .withMessage("Pašalinkite negalimus simbolius, galimi - raidės, (-), (')")
-      .trim()
-      .escape(),
-    body("purchaser.town")
-      .notEmpty()
-      .withMessage("Privalomas laukas")
-      .bail()
-      .isLength({ min: 2, max: 50 })
-      .withMessage("Gatvės pavadinimas gali būti nuo 2 iki 50 simbolių.")
-      .bail()
-      .isString()
-      .matches(/^[a-zA-ZąčęėįšųūžĄČĘĖĮŠŲŪŽ' -]+$/)
-      .withMessage("Pašalinkite negalimus simbolius, galimi - raidės, (-), (')")
-      .trim()
-      .escape(),
-    body("purchaser.postCode")
-      .notEmpty()
-      .withMessage("Privalomas laukas")
-      .bail()
-      .isString()
-      .replace()
-      .matches(/^[A-Z]{2}\d{4,10}$/)
-      .withMessage("Pašto kodo pavyzdys LT01234"),
-    // sanitizeBody("notifyOnReply").toBoolean(),
-  ],
-  async (req, res) => {
-    console.log("Request received on /api/order");
-
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    const orderNo = uuidv4();
-
-    const { products, purchaser, paymentStatus } = req.body;
-    console.log(
-      "Payload being sent to Google Sheets:",
-      purchaser.firstName,
-      purchaser.termsConfirmed,
-      products[0].title
-    );
-
-    const arrOfProducts = [];
-    products.forEach((product) => {
-      arrOfProducts.push([
-        product.id,
-        product.title,
-        product.quantity,
-        product.totalPrice,
-      ]);
-    });
-    console.log("arrOfProducts: ", arrOfProducts);
-
-    const orderValues = (arr1, arr2, status) => {
-      let arr = [];
-      for (const element of arr2) {
-        arr.push(arr1.concat(element).concat(status));
-      }
-      return arr;
-    };
-
-    try {
-      await sheets.spreadsheets.values.append({
-        spreadsheetId: PRODUCT_LIST_ID,
-        range: "Orders!A2",
-        valueInputOption: "USER_ENTERED",
-        insertDataOption: "INSERT_ROWS",
-        resource: {
-          majorDimension: "ROWS",
-          values: orderValues(
-            [
-              DOMPurify.sanitize(purchaser.firstName),
-              DOMPurify.sanitize(purchaser.lastName),
-              DOMPurify.sanitize(purchaser.phone),
-              DOMPurify.sanitize(purchaser.email),
-              DOMPurify.sanitize(purchaser.street),
-              DOMPurify.sanitize(purchaser.town),
-              DOMPurify.sanitize(purchaser.postCode),
-              orderNo,
-              purchaser.termsConfirmed,
-            ],
-            arrOfProducts,
-            paymentStatus
-          ),
-        },
-      });
-      res.status(200).json({
-        status: 200,
-        message: "Užsakymas apdorojamas",
-        orderId: `${orderNo}`,
-        paymentStatus: "pending",
-        redirectToPayment: true,
-      });
-    } catch (error) {
-      console.error("Error updating data:", error);
-      res.status(500).json("Error updating data");
-    }
-  }
-);
-
-app.get("/api/login", (req, res) => {
-  res.render("");
-});
+// app.get("/api/login", (req, res) => {
+//   res.render("");
+// });
 
 app.post("/api/login", async (req, res) => {
   console.log("Request Body:", req.body);
