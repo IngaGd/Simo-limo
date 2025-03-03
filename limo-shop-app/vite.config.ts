3c7ef82297d127ef3ac3aca2caf44163a9c34056
IngaGd <inga.gudaite@gmail.com>
2025-03-03 19:42:04 +0700

    Add server config to vite

--- limo-shop-app/vite.config.ts
+++ limo-shop-app/vite.config.ts
@@ -2,13 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
-// https://vite.dev/config/
+
 export default defineConfig({
   base: "/",
   resolve: {
     alias: {
       components: path.resolve(__dirname, "./src/common/components"),
       assets: path.resolve(__dirname, "./src/assets"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.VITE_BASE_URL,
+        changeOrigin: true,
+        secure: true,
+      },
     },
   },
   plugins: [react(), tsconfigPaths()],
