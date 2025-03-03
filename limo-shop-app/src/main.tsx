472d809a887a07a0959c108d099baaa60ea20628
IngaGd <inga.gudaite@gmail.com>
2025-03-03 19:38:50 +0700

    Remove google auth

--- limo-shop-app/src/main.tsx
+++ limo-shop-app/src/main.tsx
@@ -2,16 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
 import { GlobalContextProvider } from "./common/context/GlobalContext.tsx";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+//import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const clientId = import.meta.env.VITE_CLIENT_ID;
+//const clientId = import.meta.env.VITE_CLIENT_ID;
 
 createRoot(document.getElementById("root")!).render(
-  <GoogleOAuthProvider clientId={clientId}>
-    <GlobalContextProvider>
-      <StrictMode>
-        <App />
-      </StrictMode>
-    </GlobalContextProvider>
-  </GoogleOAuthProvider>
+  // <GoogleOAuthProvider clientId={clientId}>
+  <GlobalContextProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </GlobalContextProvider>
+  // </GoogleOAuthProvider>
 );
