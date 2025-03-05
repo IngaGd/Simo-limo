7a4df2f187496690ab5272cfc43c2fafeaaa9668
IngaGd <inga.gudaite@gmail.com>
2025-03-05 19:34:51 +0700

    Add more links to footer

--- limo-shop-app/src/common/components/Footer/Footer.tsx
+++ limo-shop-app/src/common/components/Footer/Footer.tsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
 import styles from "./footer.module.scss";
 
-const text = "Sąlygos";
+const home = "Pagrindinis";
+const terms = "Sąlygos";
+const privacy = "Privatumo politika";
 
 export function Footer() {
   return (
     <div className={styles.footer}>
-      <Link to="/terms">{text}</Link>
+      <Link to="/">{home}</Link>
+      <Link to="/terms">{terms}</Link>
+      <Link to="/privacy">{privacy}</Link>
     </div>
   );
 }
