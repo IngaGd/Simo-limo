68f46eb2f524b675b3051ad0b72fc6c2f1a6930c
IngaGd <inga.gudaite@gmail.com>
2025-03-05 19:34:19 +0700

    Add terms

---
+++ limo-shop-app/src/assets/data/terms.ts
@@ -0,0 +1,283 @@
+export const termsData = [
+  {
+    id: 1,
+    term: "Bendrosios nuostatos",
+    conditions: [
+      {
+        id: 1.1,
+        condition: `Šios elektroninės parduotuvės taisyklės (toliau - Taisyklės) yra Pirkėjui ir Pardavėjui (toliau -
+Pardavėjas) juridiškai privalomas dokumentas, kuriame nustatomos Šalių teisės ir pareigos.`,
+      },
+      {
+        id: 1.2,
+        condition: `Pardavėjas pasilieka teisę bet kuriuo metu pakeisti, taisyti ar papildyti šias Taisykles. Apie
+Taisyklių pasikeitimus Pirkėjas informuojamas elektroninės parduotuvės tinklapyje.`,
+      },
+    ],
+  },
+  {
+    id: 2,
+    term: "Pirkimo-pardavimo sutarties sudarymas",
+    conditions: [
+      {
+        id: 2.1,
+        condition: `Pirkimo-pardavimo sutartis tarp Pirkėjo ir Pardavėjo laikoma sudaryta, kuomet Pirkėjas
+elektroninėje parduotuvėje suformuoja ir pateikia prekių užsakymą, prieš tai susipažinęs su šiomis
+taisyklėmis.`,
+      },
+    ],
+  },
+  {
+    id: 3,
+    term: "Pardavėjo teisės",
+    conditions: [
+      {
+        id: 3.1,
+        condition: `Susidarius svarbioms aplinkybėms, Pardavėjas gali laikinai arba visiškai sustabdyti elektroninės
+parduotuvės veiklą, apie tai iš anksto nepranešęs Pirkėjui.`,
+      },
+      {
+        id: 3.2,
+        condition: `Jeigu Pirkėjas bando pakenkti elektroninės parduotuvės darbui, stabilumui ir saugumui ar
+pažeidžia savo įsipareigojimus, Pardavėjas turi teisę be perspėjimo apriboti ar sustabdyti jam
+galimybę naudotis elektronine parduotuve.`,
+      },
+    ],
+  },
+  {
+    id: 4,
+    term: "Pardavėjo įsipareigojimai",
+    conditions: [
+      {
+        id: 4.1,
+        condition: `Pardavėjas įsipareigoja sudaryti galimybę naudotis elektroninės parduotuvės paslaugomis,
+kurios veikimo sąlygas nustato šios Taisyklės.`,
+      },
+      {
+        id: 4.2,
+        condition: `Pardavėjas įsipareigoja Pirkėjo įsigytas prekes pristatyti Pirkėjo pasirinktu pristatymo būdu,
+remdamasis sąlygomis, kurias nustato šios pirkimo – pardavimo Taisyklės.`,
+      },
+      {
+        id: 4.3,
+        condition: `Kai dėl nenumatytų aplinkybių Pardavėjas negali pristatyti elektroninėje parduotuvėje užsakytas
+prekes, Pardavėjas turi teisę nutraukti Pirkimo-pardavimo sutartį prieš tai informavęs pirkėją.
+Pardavėjas gali pasiūlyti analogišką prekę. Pirkėjui atsisakius, Pardavėjas įsipareigoja grąžinti
+sumokėtus pinigus per 3 darbo dienas.`,
+      },
+      {
+        id: 4.4,
+        condition: `Pirkėjui pasinaudojus Taisyklių 5.2. punkte numatyta teise, Pardavėjas įsipareigoja grąžinti
+Pirkėjui sumokėtus pinigus per 5 (penkias) darbo dienas, nuo grąžinamos prekės gavimo dienos.`,
+      },
+      {
+        id: 4.5,
+        condition: `Pardavėjas gali negrąžinti Pirkėjui jo sumokėtos pinigų sumos tol, kol prekės negrąžintos
+Pardavėjui arba kol Pirkėjas pateikia įrodymą, kad prekės yra išsiųstos Pardavėjui, atsižvelgiant į tai,
+kas įvyksta pirmiau.`,
+      },
+    ],
+  },
+  {
+    id: 5,
+    term: "Pirkėjo teisės",
+    conditions: [
+      {
+        id: 5.1,
+        condition:
+          "Pirkėjas turi teisę pirkti prekes elektroninėje parduotuvėje vadovaudamasis šiomis Taisyklėmis.",
+      },
+      {
+        id: 5.2,
+        condition: `Pirkėjas turi teisę atsisakyti elektroninėje parduotuvėje sudarytos prekių pirkimo-pardavimo
+sutarties, pranešdamas apie tai Pardavėjui raštu ne vėliau kaip per 14 (keturiolika) kalendorinių
+dienų nuo prekės pristatymo dienos.`,
+      },
+      {
+        id: 5.3,
+        condition: `5.2 punkte aptarta Pirkėjo teise Pirkėjas gali pasinaudoti tik tuo atveju, jeigu prekė nebuvo
+sugadinta arba iš esmės nepasikeitė jos išvaizda, taip pat jei ji nebuvo naudojama.`,
+      },
+    ],
+  },
+  {
+    id: 6,
+    term: "Pirkėjo įsipareigojimai",
+    conditions: [
+      {
+        id: 6.1,
+        condition: `Pirkėjas privalo sumokėti už įsigytas prekes ir jas priimti šiose Taisyklėse numatyta tvarka.`,
+      },
+      {
+        id: 6.2,
+        condition: `Jeigu Pirkėjas, prekių pristatymo metu, be svarbių priežasčių atsisako priimti prekes, Pardavėjui
+pareikalavus Pirkėjas privalo padengti prekių grąžinimo išlaidas.`,
+      },
+      {
+        id: 6.3,
+        condition: `Prekių pristatymo Pirkėjui metu Pirkėjas privalo kartu su Pardavėju arba su asmeniu pristačiusiu
+siuntą patikrinti siuntos ir prekės(-ių) būklę.`,
+      },
+      {
+        id: 6.4,
+        condition: `Pirkėjas, naudodamasis elektronine parduotuve, patvirtina, kad sutinka su šiomis pirkimo –
+pardavimo taisyklėmis ir privalo jų laikytis.`,
+      },
+    ],
+  },
+  {
+    id: 7,
+    term: "Prekių kainos",
+    conditions: [
+      {
+        id: 7.1,
+        condition: `Prekių kainos elektroninėje parduotuvėje nurodomos eurais. Kainos nurodomos su PVM.
+Patvirtinus užsakymą prie bendros prekių sumos prisideda pristatymo mokestis, jeigu toks yra
+taikomas.`,
+      },
+    ],
+  },
+  {
+    id: 8,
+    term: "Prekių užsakymas ir apmokėjimas",
+    conditions: [
+      {
+        id: 8.1,
+        condition: `Pirkėjas, apsilankęs elektroninėje parduotuvėje, išsirenka jam patinkančias prekes, kurias
+įsideda į pirkinių krepšelį. Po pirkinių krepšelio suformavimo, Pirkėjas suveda asmens duomenis,
+būtinus užsakymo įvykdymui: savo vardą, pavardę, adresą, kuriuo bus pristatomos prekės, telefono
+numerį bei papildomą informaciją, kuri gali būti svarbi pristatant užsakytas prekes. Pirkėjas patvirtiną
+jog susipažino su šiomis taisyklėmis ir patvirtina užsakymą.`,
+      },
+      {
+        id: 8.3,
+        condition: `Atsiskaityti galima naudojantis Swed, Seb, Luminor, Citadelės, Šiaulių banko elektroninės
+bankininkystės paslaugomis, bei Visa / MasterCard mokėjimo kortelėmis. Atsiskaitymai galimi euro
+valiuta. Mokėjimai apdorojami naudojantis MakeCommerce.lt mokėjimų platforma.`,
+      },
+    ],
+  },
+  {
+    id: 9,
+    term: "Prekių pristatymas",
+    conditions: [
+      {
+        id: 9.1,
+        condition: `Pirkėjas, užsakydamas prekes, privalo nurodyti tikslų prekių pristatymo adresą.`,
+      },
+      {
+        id: 9.2,
+        condition: `Pirkėjas privalo priimti prekes pats. Jeigu Pirkėjas prekių priimti pats negali, bet prekės
+pristatytos Pirkėjo nurodytu adresu, Pirkėjas neturi teisės reikšti pretenzijų dėl pristatytų prekių
+netinkamam asmeniui.`,
+      },
+      {
+        id: 9.3,
+        condition: `Pardavėjas pristato prekes Pirkėjui vadovaudamasis prekių aprašymuose nurodytais terminais.`,
+      },
+      {
+        id: 9.4,
+        condition: `Visų parduodamų prekių ypatybės nurodomos prie kiekvienos prekės esančiame aprašyme.
+Pardavėjas neatsako už tai, jog elektroninėje parduotuvėje esančių prekių spalva, forma ar kiti
+parametrai gali neatitikti realaus prekių dydžio, formos ir spalvos dėl Pirkėjo naudojamo monitoriaus
+ypatybių.`,
+      },
+      {
+        id: 9.5,
+        condition: `Pirkėjui atsisakant prekės pirkimo-pardavimo sutarties, Pirkėjui grąžinamos visos jo sumokėtos
+sumos, įskaitant Pirkėjo sumokėtas prekės pristatymo išlaidas.`,
+      },
+    ],
+  },
+  {
+    id: 10,
+    term: "Prekių grąžinimas",
+    conditions: [
+      {
+        id: 10.1,
+        condition: `Kokybiškos prekės gali būti grąžinamos per 14 dienų nuo jų gavimo dienos. Parduotų
+nekokybiškų prekių trūkumai šalinami, nekokybiškos prekės keičiamos, grąžinamos
+vadovaujantis Lietuvos Respublikos civilinio kodekso nuostatomis.`,
+      },
+      {
+        id: 10.2,
+        condition: `Grąžinant prekes būtina laikytis šių sąlygų:`,
+        details: [
+          {
+            id: 1,
+            index: "10.2.1",
+            condition: `Grąžinama prekė turi būti originalioje tvarkingoje pakuotėje (šis punktas netaikomas tuo
+atveju, kai grąžinama nekokybiška prekė);`,
+          },
+          {
+            id: 2,
+            index: "10.2.2",
+            condition: `Prekė turi būti Pirkėjo nesugadinta;`,
+          },
+          {
+            id: 3,
+            index: "10.2.3",
+            condition: `Prekė turi būti nenaudota, nepraradusi prekinės išvaizdos: nepažeistos
+etiketės, nenuplėštos apsauginės plėvelės ir kt. (šis punktas netaikomas tuo atveju, kai
+grąžinama nekokybiška prekė);`,
+          },
+          {
+            id: 4,
+            index: "10.2.4",
+            condition: `Grąžinama prekė turi būti tos pačios komplektacijos, kokios buvo Pirkėjo gauta (šis punktas
+netaikomas tuo atveju, kai grąžinama nekokybiška prekė);`,
+          },
+          {
+            id: 5,
+            index: "10.2.5",
+            condition: `Pardavėjas turi teisę atsisakyti priimti Pirkėjo grąžinamas prekes, jei buvo nesilaikoma
+prekių grąžinimo sąlygų;`,
+          },
+          {
+            id: 6,
+            index: "10.2.6",
+            condition: `Prekių grąžinimas vykdomas Pardavėjo nustatytu būdu per 14 (keturiolika) dienų nuo prekių
+gavimo dienos (šis punktas netaikomas tuo atveju, kai prekė nekokybiška - tokiu atveju vykdomas
+prekių grąžinimas per įstatyminį garantinį terminą);`,
+          },
+        ],
+      },
+      {
+        id: 10.3,
+        condition: `Tinkamos kokybės prekių grąžinimas ir keitimas vykdomas vadovaujantis Civilinio kodekso
+6.22810 straipsnio 1 dalimi;`,
+      },
+      {
+        id: 10.4,
+        condition: `Už Pirkėjo priimtas, bet vėliau atsisakytas prekes sumokėti pinigai grąžinami į Pirkėjo sąskaitą
+ne vėliau kaip per 5 (penkias) dienas nuo prekės grąžinimo Pardavėjui dienos, nebent Pardavėjas ir
+Pirkėjas susitaria kitaip.`,
+      },
+    ],
+  },
+  {
+    id: 11,
+    term: "Baigiamosios nuostatos",
+    conditions: [
+      {
+        id: 11.1,
+        condition: `Šios Taisyklės sudarytos vadovaujantis Lietuvos Respublikos teisės aktais.`,
+      },
+      {
+        id: 11.2,
+        condition: `Šioms Taisyklėms ir santykiams tarp Šalių šių Taisyklių atžvilgiu taikomos ir jos aiškinamos
+remiantis Lietuvos Respublikos įstatymais.`,
+      },
+      {
+        id: 11.3,
+        condition: `Visi nesutarimai, kilę dėl šių Taisyklių vykdymo, sprendžiami derybų būdu. Nepavykus susitarti,
+ginčai sprendžiami Lietuvos Respublikos įstatymų nustatyta tvarka.`,
+      },
+      {
+        id: 11.4,
+        condition: `Šalys atleidžiamos nuo įsipareigojimų pagal šias Taisykles vykdymo, jei jų įvykdyti negalima
+dėl nenumatytų ir Šalių valios nepriklausančių aplinkybių (Force major).`,
+      },
+    ],
+  },
+];
--- limo-shop-app/src/pages/Terms/Terms.tsx
+++ limo-shop-app/src/pages/Terms/Terms.tsx
@@ -1,3 +1,36 @@
+import { termsData } from "../../assets/data/terms";
+import styles from "./terms.module.scss";
+
 export function Terms() {
-  return <div>Terms</div>;
+  return (
+    <div className={styles.terms}>
+      <div className={styles.heading}>Pirkimo sąlygos</div>
+      {termsData?.map((term) => (
+        <div key={term.id} className={styles.paragraph}>
+          <div className={styles.numeration}>
+            <div>{term.id}.</div>
+            <div>{term.term}</div>
+          </div>
+          <div>
+            {term.conditions.map((condition) => (
+              <div key={condition.id}>
+                <div>
+                  {condition.id}. {condition.condition}
+                </div>
+                <div>
+                  {condition.details?.map((detail) => (
+                    <div key={detail.id}>
+                      <div>
+                        {detail.index}. {detail.condition}
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
