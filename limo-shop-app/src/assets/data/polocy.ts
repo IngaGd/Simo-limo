84717821768028824f2f262073f8f32348a92aad
IngaGd <inga.gudaite@gmail.com>
2025-03-27 17:35:43 +0700

    Refactoring

--- limo-shop-app/src/assets/data/polocy.ts
+++ limo-shop-app/src/assets/data/polocy.ts
@@ -5,11 +5,11 @@ export const policyData = [
     paragraphs: [
       {
         id: 1,
-        content: `Jums įsigijus prekę mūsų elektroninėje parduotuvėje, mes ir jūs sudarome pirkimo-pardavimo sutartį, kurios įgyvendinimui reikalinga informacija apie jus: vardas, pavardė, tel. nr., el. pašto adresas ir, pasirinkus pristatymą į namus, adresas. Jūsų kontaktai yra reikalingi tam, kad informuotume apie užsakymo eigą ir kitiems su užsakymu susijusiems klausimams. Ši asmeninė informacija yra reikalinga tik konkrečiai sutarčiai sudaryti ir įgyvendinti.`,
+        content: `Jums įsigijus prekę mūsų elektroninėje parduotuvėje, mes ir jūs sudarome pirkimo-pardavimo sutartį, kurios įgyvendinimui reikalinga informacija apie jus: vardas, pavardė, tel. nr., el. pašto adresas ir, pasirinkus pristatymą į namus, adresas. Jūsų kontaktai yra reikalingi tam, kad informuotume apie užsakymo eigą ir kitiems su užsakymu susijusiems klausimams. Ši asmeninė informacija yra reikalinga tik konkrečiai sutarčiai sudaryti ir įgyvendinti.`,
       },
       {
         id: 2,
-        content: `Jeigu apsipirkdami pas mus pasirenkate susikurti paskyrą, mes saugome visą su jūsų pirkimais susijusią istoriją. Tokiu būdu pagreitiname jūsų ateities apsipirkimus, nes jums nebereikia suvesti savo asmens duomenų dar kartą.`,
+        content: `Jeigu apsipirkdami pas mus pasirenkate susikurti paskyrą, mes saugome visą su jūsų pirkimais susijusią istoriją. Tokiu būdu pagreitiname jūsų ateities apsipirkimus, nes jums nebereikia suvesti savo asmens duomenų dar kartą.`,
       },
     ],
   },
@@ -19,21 +19,21 @@ export const policyData = [
     paragraphs: [
       {
         id: 1,
-        content: `Jūsų asmens duomenis mes naudojame išskirtinai tam, kad palengvintume jūsų apsipirkimą ir pateiktume kaip įmanoma aktualesnius pasiūlymus. Duomenys kuriuos analizuojame yra nuasmeninti, jų negalime susieti su konkrečiu asmeniu. Nenuasmeninti duomenys yra naudojami tik tais atvejais kai siekiama prekių grąžinimo, prekių pristatymo, mokėjimų atvejams spręsti.`,
+        content: `Jūsų asmens duomenis mes naudojame išskirtinai tam, kad palengvintume jūsų apsipirkimą ir pateiktume kaip įmanoma aktualesnius pasiūlymus. Duomenys kuriuos analizuojame yra nuasmeninti, jų negalime susieti su konkrečiu asmeniu. Nenuasmeninti duomenys yra naudojami tik tais atvejais kai siekiama prekių grąžinimo, prekių pristatymo, mokėjimų atvejams spręsti.`,
       },
     ],
   },
   {
     id: 3,
-    title: "Kaip saugome ir apdorojame asmeninius duomenis?",
+    title: "Kaip saugome ir apdorojame asmeninius duomenis?",
     paragraphs: [
       {
         id: 1,
-        content: `Su naršymu susijusius nuasmenintus duomenis kaupiame ir apdorojame Google Analytics platformoje, kurios duomenų apsaugos politiką galite perskaityti:  https://policies.google.com/privacy.`,
+        content: `Su naršymu susijusius nuasmenintus duomenis kaupiame ir apdorojame Google Analytics platformoje, kurios duomenų apsaugos politiką galite perskaityti: https://policies.google.com/privacy.`,
       },
       {
         id: 2,
-        content: `“Elektroninėje parduotuvėje apmokėjimai apdorojami naudojantis makecommerce.lt platforma, kurios valdytojas Maksekeskus AS (Liivalaia 45, Tallinn 10145, Estija, reg. nr.:12268475), todėl Jūsų asmeninė informacija, reikalinga mokėjimo įvykdymui ir patvirtinimui, bus perduodama Maksekeskus AS.”`,
+        content: `“Elektroninėje parduotuvėje apmokėjimai apdorojami naudojantis makecommerce.lt platforma, kurios valdytojas Maksekeskus AS (Liivalaia 45, Tallinn 10145, Estija, reg. nr.:12268475), todėl Jūsų asmeninė informacija, reikalinga mokėjimo įvykdymui ir patvirtinimui, bus perduodama Maksekeskus AS.”`,
       },
       {
         id: 3,
@@ -47,7 +47,7 @@ export const policyData = [
     paragraphs: [
       {
         id: 1,
-        content: `Slapukai yra nedideli tekstiniai failai, kurie atsiunčiami į Jūsų įrenginį naršant atitinkamoje interneto svetainėje. Užtikriname, kad slapukai padeda atpažinti tik Jūsų įrenginį, ne jus patį. Tiek mes, tiek visos internetinės parduotuvės  slapukus naudoja siekdamos, kad apsilankymas svetainėje būtų paprastesnis ir efektyvesnis. Jokie slapukai negali susieti informacijos su konkrečiu asmeniu, t. y. jie renka tik nuasmenintus duomenis.`,
+        content: `Slapukai yra nedideli tekstiniai failai, kurie atsiunčiami į Jūsų įrenginį naršant atitinkamoje interneto svetainėje. Užtikriname, kad slapukai padeda atpažinti tik Jūsų įrenginį, ne jus patį. Tiek mes, tiek visos internetinės parduotuvės slapukus naudoja siekdamos, kad apsilankymas svetainėje būtų paprastesnis ir efektyvesnis. Jokie slapukai negali susieti informacijos su konkrečiu asmeniu, t. y. jie renka tik nuasmenintus duomenis.`,
       },
       {
         id: 2,
