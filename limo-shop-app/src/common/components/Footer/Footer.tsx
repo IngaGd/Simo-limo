c99ebecee7c15f52620f2c597cc8c8033789ba0c
IngaGd <inga.gudaite@gmail.com>
2025-06-13 16:37:19 +0300

    Add contacts link into footer, refactor MainLayout component

--- limo-shop-app/src/common/components/Footer/Footer.tsx
+++ limo-shop-app/src/common/components/Footer/Footer.tsx
@@ -4,6 +4,7 @@ import styles from "./footer.module.scss";
 const home = "Pagrindinis";
 const terms = "Sąlygos";
 const privacy = "Privatumo politika";
+const contacts = "Kontaktai";
 
 export function Footer() {
   return (
@@ -11,6 +12,7 @@ export function Footer() {
       <Link to="/">{home}</Link>
       <Link to="/terms">{terms}</Link>
       <Link to="/privacy">{privacy}</Link>
+      <Link to="/contacts">{contacts}</Link>
     </div>
   );
 }
--- limo-shop-app/src/common/layouts/MainLayout/MainLayout.tsx
+++ limo-shop-app/src/common/layouts/MainLayout/MainLayout.tsx
@@ -3,26 +3,11 @@ import { Header } from "components/Header";
 import { Footer } from "components/Footer";
 import styles from "./mainLayout.module.scss";
 import { Suspense } from "react";
-// import { GlobalContext } from "src/common/context/GlobalContext";
-// import { GlobalContextType } from "src/common/context/globalContext.types";
-import useDateCountDown from "src/common/hooks/useDateCountDown";
+// import useDateCountDown from "src/common/hooks/useDateCountDown";
 import { ScrollToTop } from "src/common/components/ScrollToTop/ScrollToTop";
 
 export function MainLayout() {
-  // const { products } = useContext(GlobalContext) as GlobalContextType;
-  const { days, hours, minutes, seconds } = useDateCountDown();
-
-  // useEffect(() => {
-  //   if (!products || products.length === 0) return;
-  //   products.forEach((p) => {
-  //     const img = new Image();
-  //     img.src = p.imagePath;
-  //   });
-  // }, [products]);
-
-  //   const text = `Užsukai į <b>testinę</b> simo.limo puslapio versiją. Limonado jau yra.
-  // Nori atsigerti? Parašyk man - forma kontaktuose, arba numesk žinutę
-  // IG. E-parduotuvė dar fermentuojasi, iki atidarymo liko:`;
+  // const { days, hours, minutes, seconds } = useDateCountDown();
 
   return (
     <div className={styles.container}>
@@ -31,14 +16,14 @@ export function MainLayout() {
         <p>
           Užsukai į <b>testinę</b> simo.limo puslapio versiją. Limonado jau yra.
           Nori atsigerti? Parašyk man - forma kontaktuose, arba numesk žinutę
-          IG. E-parduotuvė dar fermentuojasi, iki atidarymo liko:
+          IG. E-parduotuvė dar fermentuojasi.
         </p>
-        <div className={styles.date}>
+        {/* <div className={styles.date}>
           <div>{days} d</div>
           <div>{hours} h</div>
           <div>{minutes} m</div>
           <div>{seconds} s</div>
-        </div>
+        </div> */}
       </div>
       <ScrollToTop />
       <Suspense fallback={null}>
